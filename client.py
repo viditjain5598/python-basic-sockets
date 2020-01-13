d1e5a77cd09bc0fe456817ebc4ed9fd6316f8ab5
Vidit Jain <viditjain5598@gmail.com>
2020-01-13 22:03:03 +0530

    add communication loop

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 import socket
+import time 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 msg = s.recv(1024)
 print((msg.decode("utf-8")))
+
+while True:
+    time.sleep(2)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 1234))
+    msg = s.recv(1024)
+    print((msg.decode("utf-8")))
+
+
--- server.py
+++ server.py
@@ -8,4 +8,4 @@ while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has been established!")
     clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    
+   clientsocket.close() 
