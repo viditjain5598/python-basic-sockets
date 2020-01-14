358072e8c7fc0cd0d73efc9c374f52ed40e556a4
Vidit Jain <viditjain5598@gmail.com>
2020-01-14 23:29:31 +0530

    remove client side redundancy

--- client.py
+++ client.py
@@ -1,12 +1,6 @@
 import socket
 import time 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-
-msg = s.recv(1024)
-print((msg.decode("utf-8")))
-
 while True:
     time.sleep(2)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
