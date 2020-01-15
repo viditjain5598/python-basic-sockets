33bf7ae21c4412d2b3e3ae1e8952a49bd96be05d
Vidit Jain <viditjain5598@gmail.com>
2020-01-15 20:03:45 +0530

    client side buffering

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
 import socket
 import time 
 
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((socket.gethostname(), 1234))
+
 while True:
+    full_msg = ''
     time.sleep(2)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostname(), 1234))
-    msg = s.recv(1024)
-    print((msg.decode("utf-8")))
+    s.connect((socket.gethostname(), 1234)) 
+    while True:
+        msg = s.recv(8)
+        if len(msg) <= 0:
+            break
+        full_msg += msg.decode("utf-8") 
+    print(full_msg)
 
 
