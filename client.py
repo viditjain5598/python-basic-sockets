a19d1a29c86a7ff2a5008c053cec3fa9149f08b8
Vidit Jain <viditjain5598@gmail.com>
2020-01-15 21:12:52 +0530

    implement fixed length header

--- client.py
+++ client.py
@@ -3,17 +3,29 @@ import time
 
 #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect((socket.gethostname(), 1234))
+HEADERSIZE = 10
 
 while True:
-    full_msg = ''
+    full_msg = ""
     time.sleep(2)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((socket.gethostname(), 1234)) 
+    new_msg = True
     while True:
-        msg = s.recv(8)
+        msg = s.recv(12)
         if len(msg) <= 0:
             break
-        full_msg += msg.decode("utf-8") 
-    print(full_msg)
+
+        if not new_msg:
+            full_msg += msg.decode("utf-8")
+        
+        if new_msg:
+            new_msg = False
+            msg_len = int(msg[:HEADERSIZE])
+            full_msg += msg.decode("utf-8")[HEADERSIZE:]
+
+        if len(full_msg) >= msg_len:
+            print(full_msg)
+            break
 
 
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -7,5 +9,7 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    msg = "welcome to the server, client!"
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    clientsocket.send(bytes(msg, "utf-8"))
     clientsocket.close() 
